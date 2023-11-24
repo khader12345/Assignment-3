cbe40c607a34169f27c515e03eeed4c3387b3738
Muhammed Uddin <officiallykhader@gmail.com>
2023-11-24 06:54:36 -0500

    More changes
    Many more errors were resolved in terms of variable names and the creation of a new "open_new_account" with its attributes.

--- Main Banking Application (Muhammed Khader Uddin).py
+++ Main Banking Application (Muhammed Khader Uddin).py
@@ -1,20 +1,20 @@
 
 
 class Account:
-     def __init__(self, account_code, account_holder, rate_of_interest, present_balance = 0):
-        self._account_number=account_code
+     def __init__(self, account_number, account_holder, rate_of_interest, present_balance = 0):
+        self._account_number=account_number
         self._account_holder=account_holder
         self._present_balance=present_balance
         self.rate_of_interest=rate_of_interest
 
      def get_account_number(self):
-        return self._account_code
-     
+        return self._account_number
+    
      def get_account_holder(self):
-         return self._account_user_name
+         return self._account_holder
      
      def get_rate_of_interest(self):
-         return self._interest_rate
+         return self._rate_of_interest
      
      def get_present_balance(self):
          return self._present_balance
@@ -23,7 +23,7 @@ class Account:
          self._account_user_name=recent_name
 
      def set_rate_of_interest(self, recent_rate):
-         self._interest_of_rate=recent_rate
+         self._rate_of_interest=recent_rate
       
      def deposit(self, value):
              self._present_balance+=value
@@ -34,8 +34,8 @@ class Account:
              return f"*Withdraw {value} CAD | New balance: {self._present_balance:.2f}"
         
 class savingsAccount(Account):
-    def __init__(self, account_code, account_holder, rate_of_interest, present_balance, minimum_bal):
-        super().__init__(account_code, account_holder, rate_of_interest, present_balance)
+    def __init__(self, account_number, account_holder, rate_of_interest, present_balance, minimum_bal):
+        super().__init__(account_number, account_holder, rate_of_interest, present_balance)
         self._minimum_bal=minimum_bal
 
     def withdraw(self, value):
@@ -46,8 +46,8 @@ class savingsAccount(Account):
             return"*Withdrawal rejected, insufficient funds to maintain the minimum balance*"
 
 class chequingAccount(Account):
-    def __init__(self, account_code, account_holder, rate_of_interest, present_balance, over_lim):
-        super().__init__(account_code, account_holder, rate_of_interest, present_balance)
+    def __init__(self, account_number, account_holder, rate_of_interest, present_balance, over_lim):
+        super().__init__(account_number, account_holder, rate_of_interest, present_balance)
         self._over_lim=over_lim
 
     def withdraw(self, value):
@@ -80,7 +80,33 @@ class Application:
                 break
             else:
                 print("*Invalid option please enter the correct option*")
-            
+
+    def choosing_account(self):
+        account_number=input("Please enter the account number: ")
+        account=bank.find_account(account_number)
+        if account:
+            self.presentAccount=account
+            self.Show_Account_Menu()
+        else:
+            print(*"Account not found please enter the correct account number*")
+
+    def open_new_account(self):
+        account_type=input("Please enter account type (Savings or Chequing): ")
+        account_number=input("Please enter the account number: ")
+        account_holder=input("Please enter the accounts owners name: ")
+        rate_of_interest=input("Please the rate of interest: ")
+        present_balance=input("Please enter the current balance: ")
+
+        if account_type=="Savings":
+            more_param=float(input("Please enter the minimum amount for Savings account: "))
+        elif account_type=="Chequing":
+            more_param=float(input("Please enter the overdraft limit amount for Chequings account: "))
+        else:
+            print("*Incorrect account type*")
+            return
+        
+        bank.open_new_account(account_type, account_number, account_holder, rate_of_interest, present_balance, more_param)
+        print(f"*New {account_type} account has opened successfully*")
 
     def Show_Account_Menu(self):
         while True:
